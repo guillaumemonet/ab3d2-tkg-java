0a94b23c93fe6cf1b555f0a3605b4107276db8d8
Guillaume Monet <guillaume.monet@free.fr>
2026-03-28 14:20:59 +0100

    Create LevelManager class

---
+++ src/main/java/com/ab3d2/LevelManager.java
@@ -0,0 +1,13 @@
+package com.ab3d2;
+
+public class LevelManager {
+    // Main orchestrator functions go here
+
+    public LevelManager() {
+        // Initialization code
+    }
+
+    public void loadLevel(String levelFilePath) {
+        // Load level logic
+    }
+}
