3ea7da6ae4b1696a7b6c461a474efb880b4e086c
Guillaume Monet <guillaume.monet@free.fr>
2026-03-28 14:31:39 +0100

    Add ZEdge class for edge structure with position, length, joinZoneId, and flags

---
+++ src/main/java/com/ab3d2/core/level/ZEdge.java
@@ -0,0 +1,46 @@
+public class ZEdge {
+    private Position position;
+    private double length;
+    private String joinZoneId;
+    private int flags;
+
+    public ZEdge(Position position, double length, String joinZoneId, int flags) {
+        this.position = position;
+        this.length = length;
+        this.joinZoneId = joinZoneId;
+        this.flags = flags;
+    }
+
+    // Getters and setters
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public String getJoinZoneId() {
+        return joinZoneId;
+    }
+
+    public void setJoinZoneId(String joinZoneId) {
+        this.joinZoneId = joinZoneId;
+    }
+
+    public int getFlags() {
+        return flags;
+    }
+
+    public void setFlags(int flags) {
+        this.flags = flags;
+    }
+}
