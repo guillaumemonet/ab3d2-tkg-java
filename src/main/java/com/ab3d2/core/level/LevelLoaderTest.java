6c8c86fdd7fd990358a30cbd49ed28b4fb508075
Guillaume Monet <guillaume.monet@free.fr>
2026-03-28 23:22:56 +0100

    Add LevelLoaderTest.java for level framework testing

---
+++ src/main/java/com/ab3d2/core/level/LevelLoaderTest.java
@@ -0,0 +1,9 @@
+package com.ab3d2.core.level;
+
+public class LevelLoaderTest {
+    public static void main(String[] args) {
+        System.out.println("Level Framework Test");
+        Level level = new Level();
+        System.out.println("Framework loaded successfully");
+    }
+}
