ff7a89c02f86bafb403d881bcc2b0471642f4992
Guillaume Monet <guillaume.monet@free.fr>
2026-03-28 14:41:54 +0100

    Create LevelManager.java

---
+++ src/main/java/com/ab3d2/core/level/LevelManager.java
@@ -0,0 +1,9 @@
+package com.ab3d2.core.level;
+
+public class LevelManager {
+    // Main orchestrator that loads all level files (zones, graphics, clips, collision maps)
+
+    public void loadLevel(String levelFilePath) {
+        // Implementation to load level files
+    }
+}
